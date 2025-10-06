4f377c2bb91a902ba974939aa11c3768e15080ac
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-10-06 22:12:00 +0530

    Update ClassObject1.java

--- ClassObject1.java
+++ ClassObject1.java
@@ -1,7 +1,7 @@
 public class Person{
     String name;
     int age;
-
+ 
     public Person( String name,int age) {
         this.name = name;
         this.age = age;
@@ -17,4 +17,5 @@ class ClassObject1{
         p.display();
         
     }
+
 }
